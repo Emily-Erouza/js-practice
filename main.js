4a4f963bf69d0b2667af88b9f2000e07ad2bac50
Emily-Erouza <emilysbongile17@gmail.com>
2024-09-09 08:43:08 -0400

    add js

--- main.js
+++ main.js
@@ -56,3 +56,33 @@ switch(athleteFinalPosition){
     console.log('No medal awarded.');
     break;
 }
+
+// Calling a Function
+function sayThanks(){
+  console.log('Thank you for your purchase! We appreciate your business.')
+}
+sayThanks();
+sayThanks();
+
+// Parameters and Arguments
+function sayThanks(name) {
+  console.log('Thank you for your purchase ' + name + '! We appreciate your business.');
+}
+
+sayThanks('Cole');
+// Thank you for your purchase Cole! We appreciate your business. results
+
+// making shopping lisst
+function makeShoppingList(item1 = 'milk', item2 = 'bread', item3 = 'eggs'){
+  console.log(`Remember to buy ${item1}`);
+  console.log(`Remember to buy ${item2}`);
+  console.log(`Remember to buy ${item3}`);
+}
+
+// function to help us calculate the number of monitors needed!
+function monitorCount(rows, columns) {
+  return rows * columns;
+  }
+  const numOfMonitors = monitorCount(5,4) ;
+  console.log(numOfMonitors)
+  
